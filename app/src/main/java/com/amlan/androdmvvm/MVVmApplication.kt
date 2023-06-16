5427503cfc7ecfba23a5ba1fcdbda6e878a020d3
Amlan <amlana.dwivedy@someshwara.com>
2023-06-16 17:45:51 +0530

    Build.gradles are made as KT style

---
+++ app/src/main/java/com/amlan/androdmvvm/MVVmApplication.kt
@@ -0,0 +1,8 @@
+package com.amlan.androdmvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MVVmApplication :Application() {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    val android_gradle_plugin_version by extra("8.0.1")
-}
 
 plugins {
-    id("com.android.application") version "${extra["android_gradle_plugin_version"]}" apply false
-    id("com.android.library") version "${extra["android_gradle_plugin_version"]}" apply false
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+
 }
 
